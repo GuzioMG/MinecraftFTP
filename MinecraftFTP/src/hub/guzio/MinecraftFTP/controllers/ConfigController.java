63bef088782bea4a9696769d1656032e357b5a9d
GuzioMG <star.master.123.mg@gmail.com>
2020-06-22 14:04:47 +0200

    Finished ConfigController
    Signed-off-by: GuzioMG <star.master.123.mg@gmail.com>

--- MinecraftFTP/src/hub/guzio/MinecraftFTP/controllers/ConfigController.java
+++ MinecraftFTP/src/hub/guzio/MinecraftFTP/controllers/ConfigController.java
@@ -1,40 +1,38 @@
 package hub.guzio.MinecraftFTP.controllers;
 
-import java.io.ObjectInputFilter.Config;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-//Config controller for MinecraftFTP
+//Config controller for MinecraftFTP. It's ment to be imported statically to give the user direct acces to it's content.
 
 //Copyright (C) Guzio 2020. All rights reserved.
 //License still apply, though, I just wanted to type "All rights reserved".
 //*UNLESS OTHERWISE NOTED!!!
 //(i.e. if you changed something, don't hesitate to add your name)
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 import hub.guzio.MinecraftFTP.abstracts.Controller;
 
 public class ConfigController extends Controller{
     
-    public static ConfigController instance;
+    public static ConfigController ConfigController_Instance;
 
     private FileConfiguration config;
     private JavaPlugin plugin;
     
     public ConfigController(JavaPlugin plugin){
-        instance = this;
+        ConfigController_Instance = this;
         this.plugin = plugin;
         config = plugin.getConfig();
-        config.options().copyDefaults(true);
+        getCf().options().copyDefaults(true);
         plugin.saveConfig();
     }
 
-    public static FileConfiguration get() {
-        return instance.config;
+    public static FileConfiguration getCf() {
+        return ConfigController_Instance.config;
     }
 
-    public static void set(Object object, Player player) {
-        
+    public static void setCf(Object object, String path) {
+        ConfigController_Instance.config.set(path, object);
+        ConfigController_Instance.plugin.saveConfig();
     }
 }
